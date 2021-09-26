c07c6a8aee7659ecd801ae4f45cdf898caa0f670
Danilo Martins <82211468+DanielLekala@users.noreply.github.com>
2021-09-26 21:26:42 +0200

    improved schema of To-do

--- models/ToDo.js
+++ models/ToDo.js
@@ -1,8 +1,29 @@
+const { Double } = require('bson');
 const mongoose = require('mongoose');
 
 const todoSchema = new mongoose.Schema({
-    item: String,
+    item: {
+        type: String,
+        required: true
+    },
 
+    priority: {
+        type: Number,
+        enum: [0, 1, 2],  // lowest to highest priority
+        default: 0  // priority of a new item is low by default
+    },
+
+    deadline: Number,  // the date will be an integer number representing the number of milliseconds ellapsed since midnight, January 1, 1970 UTC
+    
+    created: {
+        type: Number,  // date, in ms, when the item was created
+        required: true
+    },
+
+    lastModified: {
+        type: Number, // date, in ms, when the item was last modified
+        required: true
+    },
 });
 
 const ToDo = mongoose.model("ToDo", todoSchema);
